d9348ebc9a99c0b6a830fc6c46060d80de9f4fe6
carmendrpr <c.derojas@alumnos.upm.es>
2025-04-21 20:10:35 +0200

    shared file

--- shared_data.py
+++ shared_data.py
@@ -1,16 +1,18 @@
 # shared_data.py
 # =================== PARÁMETROS DE INICIO ===================
 HOME = [0.08466545884173958, 0.2863416203890516, 0.3500529264767416, -1.2254897161735516, -1.1406743714057417, 1.2107779703462462]
-# =================== ENTRADAS Y SALIDAS DE VISIÓN ===================
+# =================== COORDENADAS ============================
+z_catch = 0.158529264767416  # mm
+# =================== ENTRADAS Y SALIDAS DE VISIÓN ===========
 vision_output_pixel_coords = None     # (x, y) en píxeles
 vision_output_piece_number = None     # número de pieza (1 a 9)
 vision_output_rotation = None         # ángulo en grados (o radianes)
 
-# =================== ENTRADAS DE UR3 ===================
+# =================== ENTRADAS DE UR3 ========================
 ur3_current_cartesian = (0, 0, 0, 0, 0, 0)  # (x, y, z, yaw, pitch, roll)
 ur3_gripper_status = False  # True: sujeta, False: suelta
 
-# =================== SALIDAS A UR3 ===================
+# =================== SALIDAS A UR3 ==========================
 ur3_target_cartesian = None   # (x, y, z, yaw, pitch, roll)
 ur3_gripper_command = None    # True: cerrar, False: abrir
 
